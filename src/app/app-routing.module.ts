667979a3017ac218182605265e59cff567038705
jakovMitrovic <jaki.mitrovic@gmail.com>
2019-05-19 18:59:12 +0200

    Registracija

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import {StudentsComponent} from './components/students/students.component';
 import { ProfessorProfileComponent } from './components/professor-profile/professor-profile.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminComponent } from './components/admin/admin.component';
+import {RegisterComponent} from './components/register/register.component'
 const routes: Routes = [
   { path: '', redirectTo: '/admin_panel', pathMatch: 'full' },
   {path: 'students', component:StudentsComponent},
   {path:'professors', component:ProfessorsComponent},
   {path:'professor/:id', component:ProfessorProfileComponent},
   {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
   {path:'admin_panel', component:AdminComponent}
 ];
 
